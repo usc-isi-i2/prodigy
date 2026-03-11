bb31bd2fba6127e374235f9dbcdfb841d51e731d
Philipp Eibl <philipp.eibl@outlook.com>
2026-03-10 22:33:12 -0700

    c

--- data/graphs/ukr_ru/instagram/submit_build_user_embeddings.sh
+++ data/graphs/ukr_ru/instagram/submit_build_user_embeddings.sh
@@ -4,15 +4,15 @@
 #SBATCH --error=logs/%x_%j.err
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=32G
-#SBATCH --time=04:00:00
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=64G
+#SBATCH --time=02:00:00
 
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate prodigy
+export LD_PRELOAD=$CONDA_PREFIX/lib/libstdc++.so.6
 
 cd /home1/eibl/gfm/prodigy/data/graphs/ukr_ru/instagram
-
 mkdir -p logs
 
 python build_user_embeddings.py
