917aa5d5a4ac9953814900f1b3e8f2c7180fd203
Philipp Eibl <philipp.eibl@outlook.com>
2026-03-17 16:58:46 -0700

    bug

--- scripts/submit_generate_retweet_ukr_rus.sh
+++ scripts/submit_generate_retweet_ukr_rus.sh
@@ -5,9 +5,9 @@
 #SBATCH --partition=main
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=128G
-#SBATCH --time=03:00:00
+#SBATCH --time=01:00:00
 
-set -euo pipefail
+# set -euo pipefail
 
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate prodigy
@@ -16,4 +16,4 @@ cd /home1/eibl/gfm/prodigy/data/graphs/midterm
 mkdir -p logs
 mkdir -p /home1/eibl/gfm/prodigy/data/graphs/ukr_rus_twitter
 
-python --max_files 25 generate_retweet_graph.py --csv "/project2/ll_774_951/uk_ru/twitter/data/*/*.csv" --out "/home1/eibl/gfm/prodigy/data/graphs/ukr_ru/twitter/retweet_graph.pt"
+python generate_retweet_graph.py --max_files 25 --csv "/project2/ll_774_951/uk_ru/twitter/data/*/*.csv" --out "/home1/eibl/gfm/prodigy/data/graphs/ukr_ru/twitter/retweet_graph.pt"
