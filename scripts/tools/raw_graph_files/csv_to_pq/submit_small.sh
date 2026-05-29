3b66997e233ae3751f669329bb164f6e9584df88
Philipp Noah Eibl <philipp.eibl@outlook.com>
2026-05-29 11:06:59 -0700

    idk

--- scripts/tools/raw_graph_files/csv_to_pq/submit_small.sh
+++ scripts/tools/raw_graph_files/csv_to_pq/submit_small.sh
@@ -5,4 +5,6 @@ chmod +x generate_manifest.sh
 manifest=/scratch1/eibl/data/covid19_twitter/manifests/files.test50.txt
 n=$(wc -l < "$manifest")
 
+mkdir -p logs
+
 sbatch --export=ALL,MANIFEST="$manifest" --array=0-$((n - 1)) csv_to_parquet.sbatch
